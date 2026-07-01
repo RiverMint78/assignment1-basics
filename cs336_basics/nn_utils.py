b47664be77fbf0a6fd1d958caa51223d294c7520
RiverMint78 <67481978@qq.com>
2026-07-01 15:40:28 +0800

    ce, perplexity helper

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -1,5 +1,6 @@
 import einx
 import torch
+from jaxtyping import Float, Int
 from torch import Tensor
 
 
@@ -11,3 +12,15 @@ def softmax(x: Tensor, dim: int = -1) -> Tensor:
     names = [f"a{i}" for i in range(x.ndim)]
     names[dim] = f"[{names[dim]}]"
     return einx.softmax(f"{' '.join(names)} -> {' '.join(names)}", x)
+
+
+def cross_entropy(inputs: Float[Tensor, "... classes"], targets: Int[Tensor, "..."]) -> Float[Tensor, ""]:
+    max_logits = einx.max("... [classes]", inputs)
+    shifted = einx.subtract("... classes, ... -> ... classes", inputs, max_logits)
+    logsumexp = einx.sum("... [classes]", shifted.exp()).log()
+    target_logits = einx.get_at("... [classes], ... -> ...", shifted, targets)
+    losses = logsumexp - target_logits
+    return losses.mean()
+
+def perplexity(losses: Float[Tensor, "... seq_len"]) -> Float[Tensor, "..."]:
+    return einx.mean("... [seq_len]", losses).exp()
